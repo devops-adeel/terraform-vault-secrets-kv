3867ef16d991a4cda52c83bb38fa6d84287c8492
tfc_run <team-is@hashicorp.com>
2021-03-01 13:04:27 +0000

    Rendered provider configuration

---
+++ integration_tests/fixtures/provider.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    vault = {
+      source = "hashicorp/vault"
+    }
+  }
+  backend "remote" {
+    organization = "hc-implementation-services"
+
+    workspaces {
+      name = "terraform-vault-secrets-kv"
+    }
+  }
+}
+
+variable "approle_id" {}
+variable "approle_secret" {}
+
+provider "vault" {
+  auth_login {
+    namespace = "admin/terraform-vault-secrets-kv"
+    path = "auth/approle/login"
+
+    parameters = {
+      role_id   = var.approle_id
+      secret_id = var.approle_secret
+    }
+  }
+}
+
